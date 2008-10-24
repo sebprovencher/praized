456910d84eb5e7ffb52cc3564fb35b5892a3fb4d
stephane.daury <stephane.daury@f55e784b-744e-0410-bb87-7729b429520d>
2008-10-24 15:47:54 +0000

    Version 1.0.4.1: Security update in Snoopy library and latest version of OAuth lib

--- movabletype/praized-community/extlib/praized-php/PraizedXHTML/list.php
+++ movabletype/praized-community/extlib/praized-php/PraizedXHTML/list.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * List template, included in PraizedXHTML::_template()
+ * List template, included through PraizedXHTML::_template()
  *
- * @version 1.0.4
+ * @version 1.5.1
  * @package Praized
  * @subpackage XHTML
  * @author Stephane Daury
@@ -19,17 +19,13 @@ $config    = $this->_pzdxConfig;
 $xhtml .= '<ul class="praized-merchants-listing">';
 
 foreach ( $merchants as $merchant ) {
-    
     $link      = $this->_permalink($merchant);
-    $voteScore = intval($merchant->votes->score);
     
     $xhtml .= "<li id=\"praized-merchant-{$merchant->pid}\" class=\"praized-merchant\">";
-    $xhtml .= "<a class=\"praized-inline-merchant-container\" href=\"{$link}\">{$merchant->name} ";
-    $xhtml .= " <img class=\"praized-inline-merchant-arrow\" src=\"http://static.praized.com/praized-com/images/icons/up-right-green-arrow-9x9.gif\" border=\"0\" height=\"9\" width=\"9\"></span></a>";
+    $xhtml .= "<a class=\"praized-inline-merchant-container\" href=\"{$link}\">{$merchant->name}</a>";
     
-    if(isset($config['address']) && strtolower($config['address']) == 'true'){
+    if(isset($config['address']) && strtolower($config['address']) == 'true')
         $xhtml .= "<br /><i class=\"praized-merchant-inline-address\">{$merchant->location->street_address}, {$merchant->location->city->name}</i>";
-    }
     
     $xhtml .= "</li>";
 }
