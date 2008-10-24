48ef1c2a36b4a0bc8248672e6e86829c00fae5ab
stephane.daury <stephane.daury@f55e784b-744e-0410-bb87-7729b429520d>
2008-10-24 15:59:21 +0000

    Version 1.5.1: Security update in Snoopy library and latest version of OAuth lib

---
+++ tags/wordpress/praized-community/1.5.1/includes/php/functions/search.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Praized template functions/helpers/tags: miscelaneous search related functions
+ * 
+ * @version 1.5
+ * @package PraizedCommunity
+ * @subpackage TemplateFunctions
+ * @author Stephane Daury
+ * @copyright Praized Media, Inc. <http://praizedmedia.com/>
+ * @license Apache License, Version 2.0 <http://www.apache.org/licenses/LICENSE-2.0>
+ */
+    	
+/**
+ * Template function: Merchants search action url
+ * 
+ * @param boolean $echo Defines if the output should be echoed or simpy returned, defaults to TRUE
+ * @return string
+ * @since 0.1
+ */
+function pzdc_search_link($echo = TRUE) {
+    global $PraizedCommunity;
+    $out = $PraizedCommunity->link_helper("/search", 'merchant');
+    if ( $echo )
+        echo $out;
+    return $out;
+}
+
+/**
+ * Template function: Current merchants search term (?q=)
+ * 
+ * @param boolean $echo Defines if the output should be echoed or simpy returned, defaults to TRUE
+ * @return string
+ * @since 0.1
+ */
+function pzdc_search_query($echo = TRUE) {
+    global $PraizedCommunity;
+    $out = ( ! empty($_GET['q']) )
+         ? pzdc_stripper($_GET['q'])
+         : ( ( ! empty($_GET['l']) )
+             ? ''
+             : $PraizedCommunity->_config['default_query'] );
+    if ( $echo )
+        echo $out;
+    return $out;
+}
+
+/**
+ * Template function: Current merchants location query (?l=)
+ * 
+ * @param boolean $echo Defines if the output should be echoed or simpy returned, defaults to TRUE
+ * @return string
+ * @since 0.1
+ */
+function pzdc_search_location($echo = TRUE) {
+    global $PraizedCommunity;
+    $out = ( ! empty($_GET['l']) )
+         ? pzdc_stripper($_GET['l'])
+         : ( ( ! empty($_GET['q']) )
+             ? ''
+             : $PraizedCommunity->_config['default_location'] );
+    if ( $echo )
+        echo $out;
+    return $out;
+}
+
+/**
+ * Template function: Current merchants tag search term (?t= or ?tag=)
+ * 
+ * @param boolean $echo Defines if the output should be echoed or simpy returned, defaults to TRUE
+ * @return string
+ * @since 0.1
+ */
+function pzdc_search_tag($echo = TRUE) {
+    global $PraizedCommunity;
+    if ( isset($_GET['tag']) )
+        $out = pzdc_stripper($_GET['tag']);
+    else
+        $out = pzdc_stripper($_GET['t']);
+    if ( $echo )
+        echo $out;
+    return $out;
+}
+?>
